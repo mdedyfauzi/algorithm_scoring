f9f42ec795deef28ad2598279842d5f6e8f049d0
mdedyfauzi <mdedyfauzi@gmail.com>
2022-01-08 15:58:17 +0700

    add_comment

--- algorithm_test.js
+++ algorithm_test.js
@@ -1,4 +1,11 @@
+// anagram('aaz', 'zza'); => false
+// anagram('nagaram', 'anagram'); => true
+
+// define a function with two parameter to be compared
 const anagram = (str1, str2) => {
+  // use array method split to split parameter value into array
+  // and then use array method sort to sorting parameter value into descending
+  // finally use join to merge the array value again
   let a = str1.split('').sort().join('');
   let b = str2.split('').sort().join('');
   return console.log(a === b);
@@ -6,3 +13,5 @@ const anagram = (str1, str2) => {
 
 anagram('aaz', 'zza');
 anagram('nagaram', 'anagram');
+
+//credit https://stackoverflow.com/questions/23785465/javascript-anagram-comparison
